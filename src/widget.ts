60fc542fde156b0f874e3197fec9102a25e689a0
bashek <abashek@gmail.com>
2020-12-31 01:58:03 +0200

    Export types

--- src/widget.ts
+++ src/widget.ts
@@ -1,3 +1,4 @@
 export { NPWidget, OnChangeEvent } from './components/NPWidget';
 export { CitySelect } from './components/CitySelect';
 export { PointSelect } from './components/PointSelect';
+export { SettlementAddress, NPPoint } from './api/types';
